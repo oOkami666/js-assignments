ee9f2a375174f7cb9aca4449078201182f0ff27c
oOkami666 <narhot1992l2@mail.ru>
2019-08-10 11:57:26 +0300

    05-regex-tasks compleated

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -30,7 +30,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /(\{([\da-f]){8})-(([\da-f]){4}-){3}([\da-f]){12}/i;
 }
 
 
@@ -52,7 +52,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /^(pi)|s|(re)/;
 }
 
 
@@ -71,7 +71,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-  throw new Error('Not implemented');
+  return /^((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9]?)(\.|$)){4}$/;
 }
 
 
@@ -90,7 +90,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-  throw new Error('Not implemented');
+  return /(?!000)\d{3}-(?!00)\d{2}-(?!0000)\d{4}/;
 }
 
 
@@ -115,7 +115,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-  throw new Error('Not implemented');
+  return new RegExp(`^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[^_ ]{${minLength},}`);
 }
 
 module.exports = {
